e36a64dd44a7a23ed4696f13c5723c2490824129
Bryan <briananime07@gmail.com>
2014-08-23 00:05:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UniversitappBackend::Application.config.secret_token = '2892aef90a45f18222fc8a1e8ac8dd16f1fe7b17e9942126b65b120477bbd6785e1bfc878b8f0f55dd4400d3fd9e5cd9485a774808ac158fcf20c47343207581'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UniversitappBackend::Application.config.session_store :cookie_store, key: '_universitapp-backend_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UniversitappBackend::Application.config.session_store :active_record_store
